add980666ab16277fa3a474d56d56bfa56d91284
Sourish Chakraborty <sourish1978@gmail.com>
2019-11-24 18:19:41 +0800

    created synchronizer.api,synchronizer.core,synchronizer.infrastructure projects

---
+++ SystemSynchronizer/Synchronizer.Core/ApiCommunication/Bucket/CreateBucketResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Synchronizer.Core.ApiCommunication.Bucket
+{
+    public class CreateBucketResponse
+    {
+    }
+}
