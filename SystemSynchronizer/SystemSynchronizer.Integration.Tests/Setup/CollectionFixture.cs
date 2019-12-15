4d4a1546534ddf0c3dda513dbb8cc1d98aa1e6aa
Sourish Chakraborty <52107607+sochaty@users.noreply.github.com>
2019-12-16 05:20:19 +0800

    created test project (#6)
    * added repository classes,configured for AWS
    
    * code implemented for s3 bucket operations
    
    * implementing file uploads
    
    * implemented adding files(uploading multipart file support)
    
    * implemented listfiles and deletefile
    
    * implemented adding and listing jsonobjects in S3
    
    * created test project

---
+++ SystemSynchronizer/SystemSynchronizer.Integration.Tests/Setup/CollectionFixture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SystemSynchronizer.Integration.Tests.Setup
+{
+    [CollectionDefinition("api")]
+    public class CollectionFixture:ICollectionFixture<TestContext>
+    {
+    }
+}
