8cbd365a822db39822f3ac9440bc643074ca6424
Sourish Chakraborty <sourish1978@gmail.com>
2019-12-16 05:18:15 +0800

    created test project

---
+++ SystemSynchronizer/SystemSynchronizer.Integration.Tests/Setup/TestContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SystemSynchronizer.Integration.Tests.Setup
+{
+    public class TestContext : IAsyncLifetime
+    {
+        public Task DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InitializeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
